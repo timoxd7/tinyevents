04e81de7780d65f3f3b97b94dd2dac486f3191a2
timoxd7 <timoxd7@gmx.de>
2025-05-11 19:09:06 +0200

    Added test for out-of-scope variables

--- tests/TestEventQueue.cpp
+++ tests/TestEventQueue.cpp
@@ -63,3 +63,24 @@ TEST_F(TestEventQueue, MessageQeuedBeforeListenerAddedAndThenSent) {
     EXPECT_CALL(intCallback, Call(123)).Times(1);
     dispatcher.process();
 }
+
+TEST_F(TestEventQueue, MessageQueuedGoesOutOfScope) {
+    StrictMock<MockFunction<void(const int &)>> intCallback;
+
+    {
+        int i = 123;
+        dispatcher.queue(i);
+    }
+
+    {
+        int i = 456;
+        dispatcher.queue(i);
+    }
+
+    dispatcher.listen(intCallback.AsStdFunction());
+
+    EXPECT_CALL(intCallback, Call(123)).Times(1);
+    EXPECT_CALL(intCallback, Call(456)).Times(1);
+    
+    dispatcher.process();
+}
